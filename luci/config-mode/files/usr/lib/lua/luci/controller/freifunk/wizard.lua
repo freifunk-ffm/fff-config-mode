61513721f5c1e1d3a3af9c579a105a645a06f1d7
yanosz <yanosz@gmx.net>
2013-01-15 15:38:01 +0100

    Removed setting hostname from wizzard

--- luci/config-mode/files/usr/lib/lua/luci/controller/freifunk/wizard.lua
+++ luci/config-mode/files/usr/lib/lua/luci/controller/freifunk/wizard.lua
@@ -5,7 +5,6 @@ function index()
   if uci_state:get_first("config_mode", "wizard", "running", "0") == "1" then
     entry({"wizard", "welcome"}, template("freifunk-wizard/welcome"), "Willkommen", 10).dependent=false
     entry({"wizard", "password"}, form("freifunk-wizard/password"), "Passwort", 20).dependent=false
-    entry({"wizard", "hostname"}, form("freifunk-wizard/hostname"), "Hostname", 30).dependent=false
     entry({"wizard", "meshvpn"}, form("freifunk-wizard/meshvpn"), "Mesh-VPN", 40).dependent=false
     entry({"wizard", "completed"}, template("freifunk-wizard/completed"), "Fertig", 50).dependent=false
     entry({"wizard", "completed", "reboot"}, call("reboot"), "reboot", 1).dependent=false
